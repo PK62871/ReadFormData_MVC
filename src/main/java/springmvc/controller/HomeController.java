c5cdb473450230d5c52979ece7555faaaf42ce07
Prabhakar <prabhakar8102567@gmail.com>
2024-07-20 22:47:07 +0530

    Form Data Reading Various Type in Spring_MVC final

--- src/main/java/springmvc/controller/HomeController.java
+++ src/main/java/springmvc/controller/HomeController.java
@@ -1,5 +1,6 @@
 package springmvc.controller;
 
+import javax.jws.WebParam.Mode;
 import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.stereotype.Controller;
@@ -24,27 +25,31 @@ public class HomeController {
 		return "contact";
 	}
 
-//	@RequestMapping(path = "/registration",method = RequestMethod.POST)
-// public String recieveData(HttpServletRequest request) {
+	 //@RequestMapping(path = "/registration",method = RequestMethod.POST)
+  //public String recieveData(HttpServletRequest request,Model model) {
+		
+		
+		// 1st Way to read form data
+//		String email = request.getParameter("email");
+//		System.out.println(email);
+//		
+//		String userName = request.getParameter("userName");
+//		System.out.println(userName);
 //		
 //		
-//		// 1st Way to read form data
-////		String email = request.getParameter("email");
-////		System.out.println(email);
-////		
-////		String parameter = request.getParameter("userName");
-////		System.out.println(parameter);
-////		
-////		
-////		String parameter2 = request.getParameter("password");
-////		System.out.println(parameter2);
+//		String password = request.getParameter("password");
 //		
-//		
-//		
-//	return null;
+//		System.out.println(password);
+//		model.addAttribute("name",userName);
+//		model.addAttribute("email",email);
+//		model.addAttribute("password",password );
+//		return "success";
+		
+		
+	
 
 	// 2nd Way to read From Data By USing @RequestParam.
-//	
+	
 //	@RequestMapping(path = "/registration",method = RequestMethod.POST)
 //	public String recieveData(
 //			@RequestParam("email") String email,
@@ -61,12 +66,12 @@ public class HomeController {
 //		System.out.println(user);
 //		model.addAttribute("user",user);
 //		
-////		System.out.println(email);
-////		System.out.println(userName);
-////		System.out.println(password);
-////		model.addAttribute("name",userName);
-////		model.addAttribute("email",email);
-////		model.addAttribute("password",password);
+//		System.out.println(email);
+//		System.out.println(userName);
+//		System.out.println(password);
+//		model.addAttribute("name",userName);
+//		model.addAttribute("email",email);
+//		model.addAttribute("password",password);
 //		return "success";
 
 	
