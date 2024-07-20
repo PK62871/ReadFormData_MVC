40e01ca07c86786b0867955335777427567d3174
Prabhakar <prabhakar8102567@gmail.com>
2024-07-20 22:32:52 +0530

    Form Data Reading Various Type in Spring_MVC

---
+++ src/main/java/springmvc/controller/HomeController.java
@@ -0,0 +1,82 @@
+package springmvc.controller;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import springmvc.model.User;
+
+@Controller
+public class HomeController {
+
+	@RequestMapping("/home")
+	public String homePage() {
+		return "home";
+	}
+
+	@RequestMapping("/contact")
+	public String viewFormPage() {
+		return "contact";
+	}
+
+//	@RequestMapping(path = "/registration",method = RequestMethod.POST)
+// public String recieveData(HttpServletRequest request) {
+//		
+//		
+//		// 1st Way to read form data
+////		String email = request.getParameter("email");
+////		System.out.println(email);
+////		
+////		String parameter = request.getParameter("userName");
+////		System.out.println(parameter);
+////		
+////		
+////		String parameter2 = request.getParameter("password");
+////		System.out.println(parameter2);
+//		
+//		
+//		
+//	return null;
+
+	// 2nd Way to read From Data By USing @RequestParam.
+//	
+//	@RequestMapping(path = "/registration",method = RequestMethod.POST)
+//	public String recieveData(
+//			@RequestParam("email") String email,
+//			@RequestParam("userName")String userName,
+//			@RequestParam("password") String password,
+//			Model model
+//			) {
+//		
+//		User user = new User();
+//		user.setEmail(email);
+//		user.setUserName(userName);
+//		user.setPassword(password);
+//		
+//		System.out.println(user);
+//		model.addAttribute("user",user);
+//		
+////		System.out.println(email);
+////		System.out.println(userName);
+////		System.out.println(password);
+////		model.addAttribute("name",userName);
+////		model.addAttribute("email",email);
+////		model.addAttribute("password",password);
+//		return "success";
+
+	
+	
+	// 3rd Way to read From Data By USing @ModelAttribute.
+	@RequestMapping(path = "/registration", method = RequestMethod.POST)
+	public String recieveData(@ModelAttribute User user,Model model) {
+		System.out.println(user);
+		model.addAttribute("user",user);
+		return "success";
+
+	}
+}
---
+++ src/main/webapp/WEB-INF/views/success.jsp
@@ -0,0 +1,19 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+    pageEncoding="ISO-8859-1"%>
+   <%@ page isELIgnored="false" %>
+
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="ISO-8859-1">
+<title>Insert title here</title>
+</head>
+<body>
+
+
+<h2>Welcome:- ${user.userName}</h2>
+<h2>Your Email is:- ${user.email } </h2>
+<h2>Your Password is:- ${user.password}</h2>
+
+</body>
+</html>
